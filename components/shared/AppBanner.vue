9ce0db03f4f89c1f68e4cb759bdeb04ef061065d
AP-Porag <smauhp@gmail.com>
2022-10-02 23:41:42 +0600

    updated

--- components/shared/AppBanner.vue
+++ components/shared/AppBanner.vue
@@ -43,7 +43,7 @@ export default {
           uppercase
         "
       >
-        Hi, Iam Stoman
+        Hi, Iam Asraf
       </h1>
       <p
         class="
@@ -58,7 +58,7 @@ export default {
           text-gray-400
         "
       >
-        A Full-Stack Developer & Design Enthusiast
+        A Full-Stack Developer
       </p>
       <div class="flex justify-center sm:block">
         <a
